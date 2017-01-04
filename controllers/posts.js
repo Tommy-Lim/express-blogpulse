07b27da23dd493601481d0f9db35debef36338bc
Tommy-Lim <tlim24@gmail.com>
2017-01-04 11:26:05 -0800

    added tags to ejs

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,12 +4,47 @@ var router = express.Router();
 
 // POST /posts - create a new post
 router.post('/', function(req, res) {
+  console.log('req body is: ',req.body);
+
+  // make the comma seperted list into array
+  var tags = [];
+  if(req.body.tags){
+     tags = req.body.tags.split(',');
+    console.log(tags);
+  } else{
+    console.log('NO TAGS');
+  }
+
+  // check if the title is greater than 2 letters
+  if(!req.body.title || req.body.title.trim().length <2){
+    res.send("Error: please include a title containing at least 3 letters");
+  }
+
+  // check if the content is greater than
+  if(!req.body.content || req.body.content.trim().length < 5){
+    res.send("Error: please include content containing at least 5 letters");
+  }
+
   db.post.create({
     title: req.body.title,
     content: req.body.content,
-    authorId: req.body.authorId
+    authorId: req.body.authorId,
   })
   .then(function(post) {
+    if(tags.length > 0){
+      //add tags here
+      tags.forEach(function(tag){
+        db.tag.findOrCreate({
+          where: {name: tag}
+        })
+        .spread(function(newTag, wasCreated){
+          if(newTag){
+            // add the relationship in the third table, post_tag
+            post.addTag(newTag);
+          }
+        });
+      }); // and of forEach
+    }
     res.redirect('/');
   })
   .catch(function(error) {
@@ -32,11 +67,14 @@ router.get('/new', function(req, res) {
 router.get('/:id', function(req, res) {
   db.post.find({
     where: { id: req.params.id },
-    include: [db.author,db.comment],
+    include: [db.author,db.comment,db.tag],
   })
   .then(function(post) {
     if (!post) throw Error();
-    res.render('posts/show', { post: post });
+    // post.getTags().then(function(tags){
+    //   res.render('posts/show', { post:post, tags:tags });
+    // });
+    res.render('posts/show', { post:post });
   })
   .catch(function(error) {
     res.status(400).render('main/404');
